3d8471e314e94d07a2bbeb24f11137b9eaf73283
Bishwarup Biswas <bishwarupbiswas07@gmail.com>
2025-09-30 10:03:34 +0000

    day 11

---
+++ codewithharry/day_11.py
@@ -0,0 +1,19 @@
+name = "harry"
+friend = "johnas"
+anotherfriend = "martha"
+apple ='''he said,
+hi harry
+hey iam good
+"i want to eat an apple'''
+
+print("hello, " + name)
+#print(apple)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+#print(name[5]) #Throws an error
+
+for ch in name:
+    print(ch)
